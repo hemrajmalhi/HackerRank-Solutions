e514f408fce2ba7dbe8b762474df817821a659b7
Hemraj Malhi <hemrajmalhi1234@gmail.com>
2023-07-24 12:04:31 +0500

    Day-3

---
+++ HackerRank/Pro_2.java
@@ -0,0 +1,19 @@
+/**
+ * Scanner Class
+ */
+
+package HackerRank;
+import java.util.*;
+public class Pro_2 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            String myString = scanner.next();
+            int myInt = scanner.nextInt();
+            scanner.close();
+
+            System.out.println("myString is: " + myString);
+            System.out.println("myInt is: " + myInt);
+
+        }
+    }
+
