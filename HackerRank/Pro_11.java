e35720e0a969f970dec1709dca555ae889f85fd0
Hemraj Malhi <hemrajmalhi1234@gmail.com>
2023-07-25 22:36:01 +0500

    day-5

---
+++ HackerRank/Pro_11.java
@@ -0,0 +1,19 @@
+package HackerRank;
+import java.util.*;
+public class Pro_11 {
+        public static void main(String[] args) {
+
+
+                Scanner in = new Scanner(System.in);
+                int n = in .nextInt();
+                in.close();
+                //String s=???; Complete this line below
+                String s=String.valueOf(n);
+                if (n == Integer.parseInt(s)) {
+                    System.out.println("Good job");
+                } else {
+                    System.out.println("Wrong answer.");
+                }
+            }
+        }
+
