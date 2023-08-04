f3c9d367a880212b2a1a569a42a5fa9a4e7523d0
Hemraj Malhi <hemrajmalhi1234@gmail.com>
2023-08-04 14:23:37 +0500

    day-16

--- HackerRank/Pro_27.java
+++ HackerRank/Pro_27.java
@@ -1,7 +1,25 @@
 package HackerRank;
 
+/*
+* this question ask me impetus company of hyderabad */
 public class Pro_27 {
+
+    public  static boolean isVowel(char ch) {
+        char cha=Character.toLowerCase(ch);
+       return cha=='a'||ch=='e'|| ch=='i'|| ch=='o'|| ch=='u';
+    }
     public static void main(String[] args) {
-        System.out.println("this is problem no 27.....");
+        String word="Pakistan";
+        String consonants="";
+        word=word.toLowerCase();
+        for (int i=0;i<word.length();i++){
+           char ch=word.charAt(i);
+            System.out.println(ch);
+           if (!isVowel(ch)){
+              consonants+=ch;
+
+             }
+        }
+        System.out.println(consonants);
     }
 }
