e0e746f2d7ee54d55c276f2f7822f6185541e660
Hemraj Malhi <hemrajmalhi1234@gmail.com>
2023-08-05 10:06:18 +0500

    day-17

--- HackerRank/Pro_26.java
+++ HackerRank/Pro_26.java
@@ -1,7 +1,26 @@
 package HackerRank;
 
+import java.security.spec.ECField;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Pro_26 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner sc =new Scanner(System.in);
+
+        try {
+            int x =sc.nextInt();
+        int y= sc.nextInt();
+
+            int z=x/y;
+            System.out.println(z);
+
+        }
+        catch (InputMismatchException e ){
+            System.out.println(e);
+        }
+        catch (ArithmeticException e){
+            System.out.println(e);
+        }
     }
 }
