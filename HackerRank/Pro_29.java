e82dfb240ff2e4d1fa36ff9e41ec16b961c6d68e
Hemraj Malhi <hemrajmalhi1234@gmail.com>
2023-08-07 20:31:15 +0500

    day-20

--- HackerRank/Pro_29.java
+++ HackerRank/Pro_29.java
@@ -2,6 +2,6 @@ package HackerRank;
 
 public class Pro_29 {
     public static void main(String[] args) {
-        System.out.println("this is program number 30....");
+        System.out.println("this is program number 29....");
     }
 }
---
+++ HackerRank/Pro_30.java
@@ -0,0 +1,7 @@
+package HackerRank;
+
+public class Pro_30 {
+    public static void main(String[] args) {
+        System.out.println("this is program no 30");
+    }
+}
